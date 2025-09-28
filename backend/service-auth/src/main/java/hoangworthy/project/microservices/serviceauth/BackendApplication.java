2243ae8a0648d915dff66a6c6a4b2ddd8b1c2f9d
Đặng Minh Hoàng <Hoangsusp@gmail.com>
2025-09-29 02:47:02 +0700

    Fixing

--- backend/service-auth/src/main/java/hoangworthy/project/microservices/serviceauth/BackendApplication.java
+++ backend/service-auth/src/main/java/hoangworthy/project/microservices/serviceauth/BackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCaching
-@EnableDiscoveryClient
 public class BackendApplication {
 
     public static void main(String[] args) {
