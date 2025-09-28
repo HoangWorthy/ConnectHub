8c7b527acc2a66f20cdd999908576511dfe21b5b
Đặng Minh Hoàng <Hoangsusp@gmail.com>
2025-09-29 03:21:41 +0700

    Fixing

--- backend/service-message/src/main/java/hoangworthy/project/microservices/servicemessage/ServiceMessageApplication.java
+++ backend/service-message/src/main/java/hoangworthy/project/microservices/servicemessage/ServiceMessageApplication.java
@@ -2,10 +2,8 @@ package hoangworthy.project.microservices.servicemessage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ServiceMessageApplication {
 
     public static void main(String[] args) {
--- backend/service-profile/src/main/java/hoangworthy/project/microservices/serviceprofile/ServiceProfileApplication.java
+++ backend/service-profile/src/main/java/hoangworthy/project/microservices/serviceprofile/ServiceProfileApplication.java
@@ -2,7 +2,6 @@ package hoangworthy.project.microservices.serviceprofile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
 
